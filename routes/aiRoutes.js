5b54a28713e5edbf124356592f1777c722d32698
Getacher-Kifilie <getacherkifilie23@gmail.com>
2026-04-05 06:12:49 +0300

    a

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -6,5 +6,5 @@ const { protect } = require('../middleware/authMiddleware');
 router.post('/chat', protect, chatWithAI);
 router.post('/roadmap', protect, generateRoadmap);
 router.post('/cv', protect, generateCV);
-
+// comment
 module.exports = router;
